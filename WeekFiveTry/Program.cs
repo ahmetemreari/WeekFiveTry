fefa9f5cedb7852eed4573112606b66cc5c6df20
AHMET EMRE ARI <ahmetemreari67@gmail.com>
2024-11-04 17:27:00 +0300

    first commit

---
+++ WeekFiveTry/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+                //girdi al
+                Console.Write("Lütfen bir sayı giriniz: ");
+                int sayi = int.Parse(Console.ReadLine());
+                int karesi = sayi * sayi;
+                //karesini al yazdir
+                Console.WriteLine($"Girdiğiniz sayının karesi: {karesi}");
+                break; // Geçerli bir sayı girildiğinde döngüden çık
+            }
+            catch (FormatException)
+            //geçersiz girişde tekrar data al
+            {
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+        }
+    }
+}
